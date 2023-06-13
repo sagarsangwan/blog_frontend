47b95b25ab38631c43aae0170fce7aeb71a1e513
sagar sangwan <iamsagarsangwan@gmail.com>
2023-06-14 01:31:54 +0530

    fronted for blog app using react

---
+++ src/components/classcomponents.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+class MyFirstClass extends Component {
+    render() {
+        return (
+            <h1>Hi, this is my first class components</h1>
+        )
+    }
+}
+
+export default MyFirstClass;
---
+++ src/components/hello.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Hi() {
+    return (
+        <h1>Hi guys</h1>
+    )
+
+
+}
+export default Hi;
