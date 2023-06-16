bdc116fac6db4b03c20b0e8e21edd7ea088a5074
sagar sangwan <iamsagarsangwan@gmail.com>
2023-06-16 16:13:20 +0530

    ??

--- src/components/CounterHook.js
+++ src/components/CounterHook.js
@@ -11,8 +11,8 @@ function CounterHook() {
         //     <button onClick={() => setCount(count + 1)} className='btn btn-primary'>Click me</button>
             <h2>{text}</h2>
             <button onClick={() => setText('text is changed')} className='btn btn-primary'>Click me</button> */}
-            <input className='form-control mt-5 ' value={info.name} onChange={e => setInfo({ name: e.target.value })} />
-            <input className='form-control mt-5 ' value={info.email} onChange={e => setInfo({ email: e.target.value })} />
+            <input className='form-control mt-5 ' value={info.name} onChange={e => setInfo({ ...info, name: e.target.value })} />
+            <input className='form-control mt-5 ' value={info.email} onChange={e => setInfo({ ...info, email: e.target.value })} />
 
             <h2 className='mt-2'>Name : {info.name}</h2>
             <h2 className='mt-2'>Email : {info.email}</h2>
